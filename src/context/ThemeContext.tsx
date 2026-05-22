0e77f8cd0deda8b1bd4cb872ae6afedb520022d2
eudehh <dbcolicchio@gmail.com>
2026-05-22 18:05:03 -0300

    feat: add LanguageContext and ThemeContext for managing language and theme state

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useState, useEffect } from "react"
+import type { ReactNode } from "react"
+
+type Theme = "light" | "dark"
+type ThemeContextType = {
+    theme: Theme
+    toggleTheme: () => void
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
+
+export function ThemeProvider({ children }: { children: ReactNode }) {
+       const [theme, setTheme] = useState<Theme>(() => {
+        return (localStorage.getItem("theme") as Theme) ?? "dark"
+       })
+
+       useEffect(() => {
+        document.documentElement.setAttribute("data=theme", theme)
+        localStorage.setItem("theme", theme)
+       }, [theme])
+
+       const toggleTheme = () => {
+           setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"))
+       }
+       return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+          {children}
+        </ThemeContext.Provider>
+       )
+    }
+
+    export function useTheme(){
+        const context = useContext(ThemeContext)
+        if (context === undefined) {
+            throw new Error("useTheme must be used within a ThemeProvider")
+        }
+        return context  
+    }
